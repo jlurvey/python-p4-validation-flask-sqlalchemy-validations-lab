7ad428f314289cad7830a6b95c53212dc15187be
Joseph Lurvey <joeylurvey@gmail.com>
2024-01-11 19:56:34 -0500

    Complete lab with all tests passing

--- server/models.py
+++ server/models.py
@@ -11,7 +11,21 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
+    # Add validators
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError
+        existing_author = Author.query.filter(db.func.lower(Author.name) == db.func.lower(name)).first()
+        if existing_author and existing_author.id != self.id:
+            raise ValueError
+        return name
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if not phone_number.isdigit() or len(phone_number) != 10:
+            raise ValueError
+        return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,8 +41,41 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
-
+    # Add validators
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError
+        
+        existing_author = Post.query.filter(db.func.lower(Post.title) == db.func.lower(title)).first()
+        
+        if existing_author and existing_author.id != self.id:
+            raise ValueError
+        
+        clickbait_words = ["Won't Believe", "Secret", "Top", "Guess"]
+        
+        if not any(word in title for word in clickbait_words):
+            raise ValueError
+        return title
+    
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError
+        return content
+    
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError
+        return summary
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        if category == 'Fiction' or category == 'Non-Fiction':
+            return category
+        else:
+            raise ValueError
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
